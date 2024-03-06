a1723ac3c76c12a34a0caf215dceabb2ffc1838f
Nipaaaa1 <Frostone224@gmail.com>
2024-03-06 16:57:50 +0700

    refactor: change color name to persian blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
           "light-slate-blue": "hsl(252, 100%, 67%)",
           "light-royal-blue": "hsl(241, 81%, 54%)",
           "violet-blue": "hsla(256, 72%, 46%, 1)",
-          "presian-blue": "hsla(241, 72%, 46%, 0)",
+          "persian-blue": "hsla(241, 72%, 46%, 0)",
         },
         neutral: {
           "pale-blue": "hsl(221, 100%, 96%)",
