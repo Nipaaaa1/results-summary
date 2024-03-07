e7101017bdfb4e4fd558ad3aaf6d4714110f932c
Nipaaaa1 <Frostone224@gmail.com>
2024-03-07 16:59:44 +0700

    feat: finish desktop design

--- src/App.vue
+++ src/App.vue
@@ -18,36 +18,44 @@ useHead({
 </script>
 
 <template>
-  <main class="flex min-h-svh w-full flex-col font-hanken-grotesk">
+  <main
+    class="flex min-h-svh w-full flex-col font-hanken-grotesk md:items-center md:justify-center"
+  >
     <div
-      class="flex min-h-80 w-full flex-col items-center gap-6 rounded-b-3xl bg-gradient-to-b from-gradient-light-slate-blue to-gradient-light-royal-blue pb-10 pt-6"
+      class="md:max-h-sm max-w-2xl overflow-clip md:flex md:rounded-3xl md:shadow-2xl md:shadow-neutral-light-lavender/40"
     >
-      <h3 class="text-xl text-white/80">Your Result</h3>
       <div
-        class="flex flex-col items-center gap-2 rounded-full bg-gradient-to-b from-gradient-violet-blue to-gradient-persian-blue px-12 py-8"
+        class="flex min-h-80 w-full flex-col items-center gap-6 rounded-b-3xl bg-gradient-to-b from-gradient-light-slate-blue to-gradient-light-royal-blue px-6 pb-10 pt-6 md:flex-1 md:rounded-3xl"
       >
-        <h1 class="text-6xl font-bold text-white">76</h1>
-        <span class="text-lg font-semibold text-neutral-light-lavender">
-          of 100
-        </span>
+        <h3 class="text-xl text-white/80">Your Result</h3>
+        <div
+          class="flex flex-col items-center gap-2 rounded-full bg-gradient-to-b from-gradient-violet-blue to-gradient-persian-blue px-12 py-8"
+        >
+          <h1 class="text-6xl font-bold text-white">76</h1>
+          <span class="text-lg font-semibold text-neutral-light-lavender">
+            of 100
+          </span>
+        </div>
+        <h2 class="text-3xl font-bold text-white">Great</h2>
+        <p
+          class="text-balance text-center text-lg text-neutral-pale-blue/85 md:text-pretty"
+        >
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
       </div>
-      <h2 class="text-3xl font-bold text-white">Great</h2>
-      <p class="text-balance text-center text-lg text-neutral-pale-blue/85">
-        You scored higher than 65% of the people who have taken these tests.
-      </p>
-    </div>
-    <div class="flex flex-col gap-4 p-8">
-      <h3 class="text-xl font-bold">Summary</h3>
-      <ul class="flex flex-col gap-4">
-        <li v-for="(result, index) in data">
-          <Category :theme="themes[index]" :data="result" />
-        </li>
-      </ul>
-      <button
-        class="rounded-full bg-neutral-dark-gray-blue py-4 text-lg text-white"
-      >
-        Continue
-      </button>
+      <div class="flex flex-col gap-4 p-8 md:flex-1">
+        <h3 class="text-xl font-bold">Summary</h3>
+        <ul class="flex flex-col gap-4">
+          <li v-for="(result, index) in data">
+            <Category :theme="themes[index]" :data="result" />
+          </li>
+        </ul>
+        <button
+          class="rounded-full bg-neutral-dark-gray-blue py-4 text-lg text-white"
+        >
+          Continue
+        </button>
+      </div>
     </div>
   </main>
 </template>
