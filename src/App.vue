a386cf4b7f0b8c8c376d9a33e033bd795459d1c5
Nipaaaa1 <Frostone224@gmail.com>
2024-03-07 16:46:45 +0700

    feat: finish mobile design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { useHead } from "@unhead/vue";
 import { data } from "./data.json";
+import Category from "./components/Category.vue";
+
+const themes = ["red", "yellow", "teal", "blue"];
 
 useHead({
   title: "vueTest",
@@ -15,7 +18,36 @@ useHead({
 </script>
 
 <template>
-  <div>
-    <h1>Hellow World</h1>
-  </div>
+  <main class="flex min-h-svh w-full flex-col font-hanken-grotesk">
+    <div
+      class="flex min-h-80 w-full flex-col items-center gap-6 rounded-b-3xl bg-gradient-to-b from-gradient-light-slate-blue to-gradient-light-royal-blue pb-10 pt-6"
+    >
+      <h3 class="text-xl text-white/80">Your Result</h3>
+      <div
+        class="flex flex-col items-center gap-2 rounded-full bg-gradient-to-b from-gradient-violet-blue to-gradient-persian-blue px-12 py-8"
+      >
+        <h1 class="text-6xl font-bold text-white">76</h1>
+        <span class="text-lg font-semibold text-neutral-light-lavender">
+          of 100
+        </span>
+      </div>
+      <h2 class="text-3xl font-bold text-white">Great</h2>
+      <p class="text-balance text-center text-lg text-neutral-pale-blue/85">
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </div>
+    <div class="flex flex-col gap-4 p-8">
+      <h3 class="text-xl font-bold">Summary</h3>
+      <ul class="flex flex-col gap-4">
+        <li v-for="(result, index) in data">
+          <Category :theme="themes[index]" :data="result" />
+        </li>
+      </ul>
+      <button
+        class="rounded-full bg-neutral-dark-gray-blue py-4 text-lg text-white"
+      >
+        Continue
+      </button>
+    </div>
+  </main>
 </template>
---
+++ src/components/Category.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import clsx from "clsx";
+import { PropType, defineProps } from "vue";
+
+interface dataType {
+  category: string;
+  score: number;
+  icon: string;
+}
+
+defineProps({
+  data: { type: Object as PropType<dataType>, required: true },
+  theme: { type: String, required: true },
+});
+</script>
+
+<template>
+  <div
+    :class="
+      clsx('flex gap-3 rounded-lg p-4', {
+        ['bg-primary-light-red/10 text-primary-light-red']: theme === 'red',
+        ['bg-primary-orangey-yellow/10 text-primary-orangey-yellow']:
+          theme === 'yellow',
+        ['bg-primary-green-teal/10 text-primary-green-teal']: theme === 'teal',
+        ['bg-primary-cobalt-blue/10 text-primary-cobalt-blue']:
+          theme === 'blue',
+      })
+    "
+  >
+    <img :src="data.icon" :alt="data.category" />
+    <span class="font-semibold">{{ data.category }}</span>
+    <span class="ml-auto text-neutral-dark-gray-blue">
+      <strong class="font-bold">{{ data.score }}</strong>
+      / 100
+    </span>
+  </div>
+</template>
